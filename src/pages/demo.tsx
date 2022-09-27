bc874250d6069d246609773fa8fd7bc256ae02fd
David Reed <reedswork@proton.me>
2022-09-26 20:42:24 -0400

    fix merge

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,20 +1,25 @@
 const Demo = () => (
-	<div className="App">
-		<header className="App-header">
-			<img src="/assets/media/logo.svg" className="App-logo" alt="logo" />
-			<p>
-				Edit <code>src/App.js</code> then reload.
-			</p>
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-			</a>
-		</header>
-	</div>
+	<>
+		<ReactHelmet>
+			<link rel="stylesheet" href="/assets/css/App.css" />
+		</ReactHelmet>
+		<div className="App">
+			<header className="App-header">
+				<img src="/assets/media/logo.svg" className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> then reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	</>
 );
 
 export default Demo;
